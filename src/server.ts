b26a22eb8838ab79f79fc2835da4631d44e9a47a
Rodrigo Barros <rodrigobarros110@hotmail.com>
2024-07-18 21:35:33 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -9,23 +9,41 @@ app.use(cors());
 app.use(express.json());
 
 interface OltOutputBody {
-    slot: string;
-    port: string;
-    ont_id: string;
-    sn: string;
-    state: string;
+    data: {
+        slot: string;
+        port: string;
+        ont_id: string;
+        sn: string;
+        state: string;
+    }[];
 }
 
 app.post(
     "/oltoutput",
     async (req: Request<{}, {}, OltOutputBody>, res: Response) => {
-        const { slot, port, ont_id, sn, state } = req.body;
+        const { data } = req.body;
 
         try {
-            const oltOutput = await prisma.oltOutput.create({
-                data: { slot, port, ont_id, sn, state },
+            const createdOutputs = [];
+            for (const item of data) {
+                const response = await prisma.oltOutput.create({
+                    data: {
+                        slot: item.slot,
+                        port: item.port,
+                        ont_id: item.ont_id,
+                        sn: item.sn,
+                        state: item.state,
+                    },
+                });
+                createdOutputs.push(response);
+            }
+
+            console.log("Dados salvos com sucesso no banco de dados.");
+
+            res.status(200).json({
+                message: "Dados salvos com sucesso no banco de dados.",
+                data: createdOutputs,
             });
-            res.json(oltOutput);
         } catch (error) {
             console.error(error);
             res.status(500).json({ error: "Erro ao criar o registro" });
